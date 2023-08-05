722b152a6d55aecb5f732f637d51ee4b1ba585a4
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-08-06 02:13:44 +0800

    updates to epoll-loop

--- src/epoll_loop.cpp
+++ src/epoll_loop.cpp
@@ -10,17 +10,63 @@ EpollLoop::EpollLoop()
     }
 }
 
-void EpollLoop::setnonblocking (const int fd)
+void EpollLoop::setnonblocking (const int & fd)
 {
-
+    int old_flags= fcntl(fd, F_GETFL, 0);
+    fcntl(fd,F_SETFL, old_flags | O_NONBLOCK);
 }
 
 void EpollLoop::set_tcp_fd(const int fd)
 {
+    setnonblocking(fd);
+    epoll_event event;
+    event.events = EPOLLIN|EPOLLET;
+    event.data.fd = fd;
+    epoll_ctl(_epfd, EPOLL_CTL_ADD, fd, &event);
 
+    _tcp_fd = fd;
 }
 
 void EpollLoop::start()
 {
+    epoll_event events[MAX_EVENTS];
+    std::cout<<"[INFO] epoll loop start\n";
+    while(1)
+    {
+        int nfds = epoll_wait(_epfd, events, MAX_EVENTS,-1);
+        for(int i =0;i<nfds, ++i)
+        {
+            epoll_event& event = events[i];
+            if(event.data.fd==_tcp_fd)
+            {
+                auto [client_fd,client_ip] = handler->handle_accept(event);
+                setnonblocking(client_fd);
+                epoll_event nevent;
+                nevent.events = EPOLLIN|EPOLLET;
+                epoll_ctl(_epfd, EPOLL_CTL_MOD,(()))
 
+            }else if(event.events & EPOLLIN)
+            {
+                handler->handle_read(event);
+                epoll_event nevent;
+                nevent.events = EPOLLOUT|EPOLLET;
+                epoll_ctl(_epfd, EPOLL_CTL_MOD,((ClientContext*)event.data.ptr)->fd,&nevent);
+            }else if(event.events & EPOLLOUT)
+            {
+                handler->handle_write(event);
+                int fd = ((ClientContext*)event.data.ptr)->fd;
+                epoll_ctl(_epfd, EPOLL_CTL_DEL,fd,NULL);
+                close(fd);
+            }else 
+            {
+                std::<<cout <<"None in epoll event type\n";
+                
+            }
+        }
+    }
+}
+
+EpollLoop::~EpollLoop()
+{
+    free(handler);
 }
